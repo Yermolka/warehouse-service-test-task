9ebcc213030f121a1105a9a4c79d36865818d45d
ermoshin.andrey2 <ermoshin.andrey2@wb.ru>
2025-07-26 17:23:42 +0300

    small docker-compose.yaml fixes

--- app/logger/__init__.py
+++ app/logger/__init__.py
@@ -6,6 +6,7 @@ is_debug = os.getenv("DEBUG", "False").lower() == "true"
 handler = StreamHandler()
 handler.setFormatter(Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s"))
 
+# Basic ugly logger, should use structured logging for better exceptions readability
 logger = getLogger("warehouse-app")
 logger.addHandler(handler)
 logger.setLevel("DEBUG" if is_debug else "INFO")
